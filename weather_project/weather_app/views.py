c0d790b4cdc9e2de2f4601dead2af796da9af582
davood <mohamadbagherdavoodi@gmail.com>
2021-01-20 13:46:02 +0530

    Importing class to view

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,5 +1,7 @@
 import requests
 from django.shortcuts import render
+from .models import City
+
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
