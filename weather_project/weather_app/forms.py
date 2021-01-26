76d44cdd05735856f33c1db5976d3630b57bcdf1
davood <mohamadbagherdavoodi@gmail.com>
2021-01-26 15:06:03 +0530

    rendering Text input using form

--- weather_project/weather_app/forms.py
+++ weather_project/weather_app/forms.py
@@ -1,8 +1,9 @@
-from django.forms import ModelForm, TextInput
+from typing import Text
+from django.forms import ModelForm, TextInput, widgets
 from .models import City
 
 class CityForm(ModelForm):
     class Meta:
         model = City
         fields = ['name']
-        
+        widgets = {'name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
