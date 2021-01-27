b260b78dcd84da48c1c58f0c7fc88d195b8616c1
davood <mohamadbagherdavoodi@gmail.com>
2021-01-27 15:28:28 +0530

    add city function completed

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -7,7 +7,8 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
 
     if request.method == 'POST':
-        print(request.POST)
+        form = CityForm(request.POST)
+        form.save()
 
     form = CityForm()
 
