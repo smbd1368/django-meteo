abbbe730ff88e30c5e0a1fdfc5bebff2568ad9ba
davood <mohamadbagherdavoodi@gmail.com>
2021-01-27 15:19:03 +0530

    DUMCOM 4

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -7,7 +7,7 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
 
     if request.method == 'POST':
-        pass
+        print(request.POST)
 
     form = CityForm()
 
