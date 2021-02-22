965a1372e416056d1fbecf4680d57da527e72d07
davood <mohamadbagherdavoodi@gmail.com>
2021-02-22 14:22:04 +0530

    Removing django-heroku

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dajngo_heroku
-import dj_database_url
-from decouple import config 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,4 +134,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
