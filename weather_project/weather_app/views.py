b4407df8e3fcdfa8d67162765894cfc5bfe58959
davood <mohamadbagherdavoodi@gmail.com>
2021-03-02 18:07:07 +0530

    search function complete

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -4,6 +4,9 @@ from .models import City
 from .forms import CityForm, SearchForm
 from django.http import HttpResponseRedirect
 
+
+
+
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
 
@@ -38,8 +41,10 @@ def index(request):
 
     
 def search(request):
-
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
+    
     if request.method == 'POST':
+        
         search_query = request.POST['name']
         search_result = City.objects.filter(name__contains=search_query)
         
