b34d3d93cdda5b14243de72c6283f977c8f4d94c
davood <mohamadbagherdavoodi@gmail.com>
2021-03-20 13:43:02 +0530

    DUMCOM 2

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -5,6 +5,9 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
     city = ''
 
+    
+
+
     r = requests.get(url.format(city)).json()
 
     city_weather = {
