b172ece9b3dc3375c7c03dd2c2fe3e5a0ab7fa51
davood <mohamadbagherdavoodi@gmail.com>
2021-03-22 13:06:06 +0530

    Creating a new model class

--- weather_project/weather_app/models.py
+++ weather_project/weather_app/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    pass
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
-    city = 'Delhi'
+    city = ''
 
     r = requests.get(url.format(city)).json()
 
