ea16aa4233a5206713c9aba36c714e9a86fd3e91
davood <mohamadbagherdavoodi@gmail.com>
2021-03-26 10:08:02 +0530

    Adding JSON data template

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -5,8 +5,14 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
     city = 'Las Vegas'
 
-    r = requests.get(url.format(city))
-    print(r.text)
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : ,
+        'temprature' : ,
+        'description' : ,
+        'icon' : ,
+    }
 
 
 
