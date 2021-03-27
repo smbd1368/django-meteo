94a42cfe3718eddd5d0e2d84fc354ae74d274709
davood <mohamadbagherdavoodi@gmail.com>
2021-03-27 17:03:11 +0530

    Adding import command

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,8 +1,15 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
     city = 'Las Vegas'
+
+    #r = request.get(url.format(city))
+    #print(r.text)
+
+
+
     return render(request, 'weather_template.html')
 
     
