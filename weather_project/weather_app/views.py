9dc799d47f9734078a138d660e57d851bfb194bc
davood <mohamadbagherdavoodi@gmail.com>
2021-06-10 16:47:03 +0530

    Adding city value

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
-    
+    city = 'Las Vegas'
     return render(request, 'weather_template.html')
 
     
