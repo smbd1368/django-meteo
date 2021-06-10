ff5e60332e9ce21d7fefbb98f9256aba78003e4e
davood <mohamadbagherdavoodi@gmail.com>
2021-06-10 16:45:05 +0530

    adding farenhite to URL

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=cee83e85f6aa300a481d0bc87650b513'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=cee83e85f6aa300a481d0bc87650b513'
+    
     return render(request, 'weather_template.html')
 
     
