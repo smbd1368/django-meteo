8aeb3e36626abcd7bc24a0b6c9dd4b0747ec577d
davood <mohamadbagherdavoodi@gmail.com>
2021-06-10 16:44:03 +0530

    Adding url for openweatherAPI

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=cee83e85f6aa300a481d0bc87650b513'
     return render(request, 'weather_template.html')
 
     
