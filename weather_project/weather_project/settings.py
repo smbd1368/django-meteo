00680db4f9992d514c6a1ddd9e0a6b59006b9ee0
davood <mohamadbagherdavoodi@gmail.com>
2021-06-20 15:29:45 +0530

    Update settings.py

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'weather_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
